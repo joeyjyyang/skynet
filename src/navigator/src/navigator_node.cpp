831d5ca809c257145a24c73d2fca4e5a3977c00e
bojanglesjoey <joey.yang1997@hotmail.com>
2019-08-05 20:06:21 -0400

    move forward

--- src/navigator/src/navigator_node.cpp
+++ src/navigator/src/navigator_node.cpp
@@ -1,32 +1,53 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include <iostream>
 
 class Navigator 
 {
 public:
 	Navigator(const ros::NodeHandle &nh) : nh_(nh)
 	{
-		vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 10);
+		vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1000);
 	}
 
-	void publishData()
+	void driveStraight(double speed, double distance, bool isForward)
 	{
-		vel_msg_.linear.x = 2;
+		if (isForward)
+		{
+			vel_msg_.linear.x = speed;
+		}
+		else
+		{
+			vel_msg_.linear.x = -speed;
+		}
 		vel_msg_.linear.y = 0;
 		vel_msg_.linear.z = 0;
-		vel_msg_.angular.x = 2;
+		vel_msg_.angular.x = 0;
 		vel_msg_.angular.y = 0;
 		vel_msg_.angular.z = 0;
 
+		double t0 = ros::Time::now().toSec();
+
+		double current_distance = 0.0;
+		ros::Rate loop_rate(100);
+	
+		while (current_distance < distance)
+		{
+			vel_pub_.publish(vel_msg_);
+			double t1 = ros::Time::now().toSec();
+			current_distance = speed * (t1-t0);
+			ros::spinOnce();
+			loop_rate.sleep();
+		}
+		vel_msg_.linear.x = 0;
 		vel_pub_.publish(vel_msg_);
 	}
-
 
 private:
 	ros::NodeHandle nh_;
 	ros::Publisher vel_pub_;
+	ros::Subscriber pose_sub_;
 	geometry_msgs::Twist vel_msg_;
-
 };
 
 int main(int argc, char *argv[])
@@ -34,15 +55,11 @@ int main(int argc, char *argv[])
 	ros::init(argc, argv, "navigator_node");
 	ros::NodeHandle nh;
 	Navigator navigator(nh);
-	
-	ros::Rate l_rate(50);
 
-	while (ros::ok())
-	{
-		navigator.publishData();
-		ros::spinOnce();
-		l_rate.sleep();
-	}		
-	
+	ros::Rate loop_rate(10);
+	loop_rate.sleep();
+
+	navigator.driveStraight(1.2, 9.5, true);
+
 	return 0;
 }
